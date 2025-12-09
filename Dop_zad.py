5af4b115a75e201066be818a0eeaeb39e62f981e
apalaguta <apalaguta2010@gmail.com>
2025-12-09 17:17:10 +0300

    Update Dop_zad.py

--- Dop_zad.py
+++ Dop_zad.py
@@ -42,19 +42,19 @@ def naz_chas(chas):
             return(ed[(chas % 10)]+"надцать часов")
         else:
             a=ed[chas%10]
-            a=a[:len(a)-1]
+            a=a[:len(a) - 1]
             return(a + "надцать часов")
     elif chas // 10 == 2:
-        tex="двадцать "+ed[chas % 10]
+        tex="двадцать "+ed [chas % 10]
     else:
         tex=ed[chas % 10 ]
         
     if chas % 10 == 1:
-        return(tex+" час")
+        return(tex + " час")
     elif chas % 10 >=2 and chas % 10 <=4:
-        return(tex+" часа")
+        return(tex + " часа")
     else:
-        return(tex+" часов")
+        return(tex + " часов")
 print("Вас приветствует программа будильник! Введети время на которое нужно поставить будильник в формате часы минуты")
 vhod_dan = input().split()
 if len(vhod_dan) == 2 and vhod_dan[0].isdigit()  and vhod_dan[1].isdigit():
@@ -62,12 +62,12 @@ if len(vhod_dan) == 2 and vhod_dan[0].isdigit()  and vhod_dan[1].isdigit():
     minuti = int(vhod_dan[1])
     if minuti >= 0 and minuti < 60 and chas>=0 and chas <= 23:
         print("Будильник установлен на",naz_chas(chas),naz_minut(minuti))
-        tek_vrem=str(datetime.now())
-        tek_vrem=tek_vrem[ tek_vrem.find(" ") : tek_vrem.rfind(":") ]
-        tek_chas=int(tek_vrem[ : tek_vrem.find(":")])
-        tek_min=int(tek_vrem[ tek_vrem.find(":") + 1:])
+        tek_vrem = str(datetime.now())
+        tek_vrem = tek_vrem[ tek_vrem.find(" ") : tek_vrem.rfind(":") ]
+        tek_chas = int(tek_vrem[ : tek_vrem.find(":")])
+        tek_min = int(tek_vrem[ tek_vrem.find(":") + 1:])
         tek_vr_min = tek_chas * 60 + tek_min
-        vr_min= chas *60 + minuti
+        vr_min= chas * 60 + minuti
         razn = vr_min - tek_vr_min
         if razn < 0:
             razn += 1440
